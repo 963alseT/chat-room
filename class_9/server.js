86b56b89d62ea5e71f2efeba1b5cda5f3a566278
963alseT <156242152+963alseT@users.noreply.github.com>
2025-10-13 14:49:41 +0100

    Update server.js

--- class_9/server.js
+++ class_9/server.js
@@ -100,7 +100,7 @@ io.on('connection', (socket) => {
                 return;
             }
         }
-        if (message.startsWith('/mute ')) {
+        if (message.startsWith('/ban ')) {
             const seconds = 30;
             muteUser(userId, seconds);
             console.log("Hello");
@@ -124,6 +124,7 @@ io.on('connection', (socket) => {
 server.listen(5000, () => {
     console.log('listening on *:5000');
 });
+
 
 
 
